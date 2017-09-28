53bf0fefe9f2f08017d5c515c63908736dd989d0
Philipp Gagel <philippgagel@ymail.com>
2017-09-29 01:34:15 +0200

    Switched from monochrome detection to the use of all three color values

--- src/main/java/de/philippgagel/facerecognition/imagemanipulation/FRManipulator.java
+++ src/main/java/de/philippgagel/facerecognition/imagemanipulation/FRManipulator.java
@@ -41,15 +41,18 @@ public class FRManipulator {
         
         for(int y = 0; y < src.getHeight(); y++){
             for(int x = 0; x < src.getWidth(); x++){
-                int value = calculatePixel(src, x, y);
-                out.setRGB(x, y, value>this.sensitivity ? Color.white.getRGB() : 0);
+                int[] resColor = calculatePixel(src, x, y);
+                
+                if(resColor!=null){      
+                    out.setRGB(x, y, new Color(resColor[0], resColor[1], resColor[2]).getRGB());
+                }
             }
         }
         
         return out;
     }
     
-    private int calculatePixel(BufferedImage img, int x, int y){
+    private int[] calculatePixel(BufferedImage img, int x, int y){
         
         int startX, startY;
         int width, height;
@@ -65,56 +68,85 @@ public class FRManipulator {
         
         if(width > 0 && height > 0){
             int[] pixels = new int[width * height * 3];
+            int[] resColor = new int[3];
             
             img.getRaster().getPixels(startX, startY, width, height, pixels);
-
+                        
             // Sum up top row
-            int tr = pixelSum(pixels, 0, 0, width, 1, width);
+            int[] tr = pixelSum(pixels, 0, 0, width, 1, width);
 
             // Sum up bottom row
-            int br = pixelSum(pixels, 0, height-1, width, 1, width);
+            int[] br = pixelSum(pixels, 0, height-1, width, 1, width);
 
             // Sum up left col
-            int lc = pixelSum(pixels, 0, 0, 1, height, width);
+            int[] lc = pixelSum(pixels, 0, 0, 1, height, width);
 
             // Sum up right col
-            int rc = pixelSum(pixels, width - 1, 0, 1, height, width);
+            int[] rc = pixelSum(pixels, width - 1, 0, 1, height, width);
 
             // Sum up top right corner
-            int trc = pixelSum(pixels, width - width/2, 0, width/2, height/2, width);
+            int[] trc = pixelSum(pixels, width - width/2, 0, width/2, height/2, width);
 
             // Sum up bottom left corner
-            int blc = pixelSum(pixels, width - width/2, height - height/2, width/2, height/2, width);
+            int[] blc = pixelSum(pixels, width - width/2, height - height/2, width/2, height/2, width);
 
             // Sum up top left corner
-            int tlc = pixelSum(pixels, 0, 0, width/2, height/2, width);
+            int[] tlc = pixelSum(pixels, 0, 0, width/2, height/2, width);
 
             // Sum up bottom right corner
-            int brc = pixelSum(pixels, 0, height - 1, width, 1, width);
+            int[] brc = pixelSum(pixels, 0, height - 1, width, 1, width);
 
-            int diffTB, diffRL, diffD1, diffD2;
+            int[] diffTB, diffRL, diffD1, diffD2;
+            
+            diffTB = new int[3];
+            diffRL = new int[3];
+            diffD1 = new int[3];
+            diffD2 = new int[3];
 
-            diffTB = Math.abs(tr-br);
-            diffRL = Math.abs(rc-lc);
-            diffD1 = Math.abs(trc-blc);
-            diffD2 = Math.abs(tlc-brc);
-
-            return Math.max(Math.max(Math.max(diffTB, diffRL), diffD1), diffD2);
+            for(int i = 0; i < 3; i++){
+                diffTB[i] = Math.abs(tr[i] - br[i]);
+                diffRL[i] = Math.abs(lc[i] - rc[i]);
+                diffD1[i] = Math.abs(trc[i] - blc[i]);
+                diffD2[i] = Math.abs(tlc[i] - brc[i]);
+                
+                if(diffTB[i] > this.sensitivity){
+                    resColor[i] += 64;
+                }
+                if(diffRL[i] > this.sensitivity){
+                    resColor[i] += 64;
+                }
+                if(diffD1[i] > this.sensitivity){
+                    resColor[i] += 64;
+                }
+                if(diffD2[i] > this.sensitivity){
+                    resColor[i] += 64;
+                }
+            }
+            
+            resColor[0] = resColor[0] > 128 ? 255 : 0;
+            resColor[1] = resColor[1] > 128 ? 255 : 0;
+            resColor[2] = resColor[2] > 128 ? 255 : 0;
+            
+            return resColor;
         }
-        return 0;
+        return null;
     }
     
-    private int pixelSum(int [] pixels, int startX, int startY, int width, int height, int maskW){
-        int res = 0;
+    private int[] pixelSum(int [] pixels, int startX, int startY, int width, int height, int maskW){
+        int[] sums = new int[3];
+        
+        sums[0] = 0;
+        sums[1] = 0;
+        sums[2] = 0;
         
         for(int y = startY; y < startY + height; y++){
             for(int x = startX; x < startX + width; x++){
-                res += pixels[x*3 + y*maskW];
-                res += pixels[x*3 + y*maskW*3 + 1];
-                res += pixels[x*3 + y*maskW*3 + 2];
+                sums[0] += pixels[x*3 + y*maskW];
+                sums[1] += pixels[x*3 + y*maskW*3 + 1];
+                sums[2] += pixels[x*3 + y*maskW*3 + 2];
             }
         }
         
-        return res / (width * height);
+        return sums;
     }
 }
