8f4ae612deeca6966f841dbbc847d69dcb5df99a
Philipp <gaph1300@stud.hs-coburg.de>
2017-10-05 11:04:28 +0200

    Commented public Methods. Changed isNewImageAvailable() so it returns the webcam status.

--- src/main/java/de/philippgagel/facerecognition/camerahandling/FRImageReceiver.java
+++ src/main/java/de/philippgagel/facerecognition/camerahandling/FRImageReceiver.java
@@ -9,15 +9,24 @@ import java.util.logging.Logger;
 
 /**
  *
+ * This class uses the default webcam of the host pc and tries to receive images from it.
+ * 
  * @author Philipp Gagel
  */
 public class FRImageReceiver {
     private static final Logger LOG = Logger.getLogger(FRImageReceiver.class.getName());
     
+    // Webcam object to receive the iamges
     private Webcam webcam;
+    // Image to save the current image until a new one is ready
     private BufferedImage currentImage;
+    // Size of the images that can be requested.
     private Dimension imageSize;
     
+    /**
+     * Creates an ImageReceiver with the highest possible resolution for the default webcam 
+     * of the system
+     */
     public FRImageReceiver(){
         webcam = Webcam.getDefault();
         
@@ -32,6 +41,11 @@ public class FRImageReceiver {
         LOG.info("Image Receiver initialized");
     }
     
+    /**
+     * This method returns the current Image of the webcam
+     * 
+     * @return 
+     */
     public BufferedImage getImage(){
         if(webcam.isImageNew()){
             return (currentImage = webcam.getImage());
@@ -39,8 +53,17 @@ public class FRImageReceiver {
         return currentImage;
     }
 
+    /**
+     * This method returns the dimensions of the videostream sent by the camera
+     * 
+     * @return 
+     */
     public Dimension getImageSize() {
         return imageSize;
+    }
+    
+    public boolean isNewImageAvailable(){
+        return webcam.isImageNew();
     }
     
     
